7f313e403997a0ad7048964b7d628058bfc395a0
TanzhihuaLeo <39336156+TanzhihuaLeo@users.noreply.github.com>
2019-05-22 17:03:41 +0800

    搭建微服务Spring Boot多模块项目
    搭建微服务Spring Boot多模块项目

---
+++ dubboOne/api/src/main/java/com/tzh/dubbo/one/api/Main.java
@@ -0,0 +1,8 @@
+package com.tzh.dubbo.one.api;
+
+/**
+ * @Author: TANZHIHUA
+ * @Date: 2019/5/22 16:26
+ */
+public class Main {
+}
---
+++ dubboOne/model/src/main/java/com/tzh/dubbo/one/model/Main.java
@@ -0,0 +1,8 @@
+package com.tzh.dubbo.one.model;
+
+/**
+ * @Author: TANZHIHUA
+ * @Date: 2019/5/22 16:25
+ */
+public class Main {
+}
